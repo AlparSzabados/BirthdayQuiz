59364e6b8e9c392a127ae3b1f6ad228e983b08a2
AlparSzabados <c3sp3nar@yahoo.com>
2016-03-18 22:25:44 +0200

    Added a URL holder for easier overview

---
+++ src/main/groovy/birthdayQuiz/UrlHolder.groovy
@@ -0,0 +1,29 @@
+package birthdayQuiz
+
+class UrlHolder {
+    static Object win1 = "http://giphy.com/gifs/applause-clapping-clap-aLdiZJmmx4OVW"
+    static Object win2 = "http://giphy.com/gifs/applause-clapping-clap-aLdiZJmmx4OVW"
+    static Object win3 = "http://giphy.com/gifs/applause-clapping-clap-aLdiZJmmx4OVW"
+    static Object lose = "http://giphy.com/gifs/applause-clapping-clap-aLdiZJmmx4OVW"
+
+    static Object exercise1 = "http://giphy.com/gifs/applause-clapping-clap-aLdiZJmmx4OVW"
+    static Object exercise2 = "http://giphy.com/gifs/applause-clapping-clap-aLdiZJmmx4OVW"
+    static Object exercise3 = "http://giphy.com/gifs/applause-clapping-clap-aLdiZJmmx4OVW"
+    static Object exercise4 = "http://giphy.com/gifs/applause-clapping-clap-aLdiZJmmx4OVW"
+    static Object exercise5 = "http://giphy.com/gifs/applause-clapping-clap-aLdiZJmmx4OVW"
+    static Object exercise6 = "http://giphy.com/gifs/applause-clapping-clap-aLdiZJmmx4OVW"
+    static Object exercise7 = "http://giphy.com/gifs/applause-clapping-clap-aLdiZJmmx4OVW"
+    static Object exercise8 = "http://giphy.com/gifs/applause-clapping-clap-aLdiZJmmx4OVW"
+    static Object exercise9 = "http://giphy.com/gifs/applause-clapping-clap-aLdiZJmmx4OVW"
+    static Object exercise10 = "http://giphy.com/gifs/applause-clapping-clap-aLdiZJmmx4OVW"
+    static Object exercise11 = "http://giphy.com/gifs/applause-clapping-clap-aLdiZJmmx4OVW"
+
+    static Object virusTab1 = "http://giphy.com/gifs/applause-clapping-clap-aLdiZJmmx4OVW"
+    static Object virusTab2 = "http://giphy.com/gifs/applause-clapping-clap-aLdiZJmmx4OVW"
+    static Object virusTab3 = "http://giphy.com/gifs/applause-clapping-clap-aLdiZJmmx4OVW"
+    static Object virusTab4 = "http://giphy.com/gifs/applause-clapping-clap-aLdiZJmmx4OVW"
+    static Object virusTab5 = "http://giphy.com/gifs/applause-clapping-clap-aLdiZJmmx4OVW"
+    static Object virusTab6 = "http://giphy.com/gifs/applause-clapping-clap-aLdiZJmmx4OVW"
+    static Object virusTab7 = "http://giphy.com/gifs/applause-clapping-clap-aLdiZJmmx4OVW"
+    static Object virusTab8 = "http://giphy.com/gifs/applause-clapping-clap-aLdiZJmmx4OVW"
+}
