b262b6ca773926317c839e7c0d384432bf90aa48
AlparSzabados <c3sp3nar@yahoo.com>
2016-03-20 14:23:27 +0200

    Cleaned up the code. Updated the exercises.

--- src/main/groovy/birthdayQuiz/BirthdayQuiz.groovy
+++ src/main/groovy/birthdayQuiz/BirthdayQuiz.groovy
@@ -1,139 +1,143 @@
 package birthdayQuiz
 
 import static birthdayQuiz.ExerciseCheck.*
+import static birthdayQuiz.ExerciseCheck.*
 import static birthdayQuiz.FolderHolder.*
 import static birthdayQuiz.FileCounter.*
 import static birthdayQuiz.PlayerInput.*
-import static birthdayQuiz.DateCheck.*
 import static birthdayQuiz.TextHolder.*
-import static birthdayQuiz.XmlChange.*
+import static birthdayQuiz.DateCheck.*
+import static birthdayQuiz.UrlHolder.*
+import static birthdayQuiz.TypoHunt.*
 import static birthdayQuiz.XmlMaker.*
 import static birthdayQuiz.OpenUrl.*
-import static UrlHolder.*
 
 class BirthdayQuiz {
     static void main(String... args) {
-        /* TODO make directory for Album */
+        folder.mkdir()
 
-        if (fileCount(folder, '.xml') == 0) {
+        if (fileCount(folder, 'xml') == 0) {
             createXml()
         }
 
-        /* First exercise */
-        switch (isExerciseAllowed(1)) {
-            case false: println task1
-                switch (isExerciseFinished(1)) {
-                    case true: println finishedMessage
-                    case false: println firstTask
-                                openUrl win1
-                                openUrl exercise1;
-                }
-        }
-
-        if (fileCount(folder, 'jpg') == 30) {
-            finishExercise(1);
-            println finishedMessage
+        if (isExerciseAllowed(1)) {
+            println task1
+            switch (isExerciseFinished(1)) {
+                case true: println finishedMessage
+                case false: printf (pictureCounter, fileCount(folder, 'jpg'))
+                    openUrl task1Link
+                    finishExercise(1)
+                    if (fileCount(folder, 'jpg') == 30) {
+                        openUrl win1Gif
+                        println finishedMessage
+                    }
+            }
         }
 
         /* Second exercise */
         if (isExerciseAllowed(2)) {
-            if (isExerciseFinished(2)) {
-                /* Do something */
-            } else {
-                /* Give exercise */
-                playerInput()
-
-                openUrl win2
-                openUrl exercise2
-
-                finishExercise(2)
+            println task2
+            switch (isExerciseFinished(2)) {
+                case true: println finishedMessage
+                case false:
+                    println pressEnter
+                    playerInput()
+                    if (fileCount(folder, 'docx') == 1) {
+                        openUrl win2Gif
+                        openUrl task2Link
+                        finishExercise(2)
+                    } else {
+                        println failMessage
+                        openUrl lose1Gif
+                    }
             }
         }
 
         /* Third exercise */
         if (isExerciseAllowed(3)) {
-            if (isExerciseFinished(3)) {
-                /* Do something */
-            } else {
-                /* Give exercise */
-                openUrl virusTab1
-                openUrl virusTab2
-                openUrl virusTab3
-                openUrl virusTab4
-                openUrl virusTab5
-                openUrl exercise3
-                openUrl virusTab6
-                openUrl virusTab7
-                openUrl virusTab8
-
-                finishExercise(3)
+            println task3
+            switch (isExerciseFinished(3)) {
+                case true: println finishedMessage;
+                case false:
+                    openUrl task3Link
+                    openUrl virusTab1
+                    openUrl virusTab2
+                    openUrl virusTab3
+                    openUrl virusTab4
+                    openUrl virusTab5
+                    openUrl virusTab6
+                    openUrl virusTab7
+                    openUrl virusTab8
+                    finishExercise(3)
             }
         }
 
         /* Fourth exercise */
         if (isExerciseAllowed(4)) {
-            if (isExerciseFinished(4)) {
-                /* Do something */
-            } else {
-                /* Give exercise */
-                playerInput()
-
-                openUrl win1
-                openUrl exercise4
-
-                finishExercise(4)
+            println task4
+            switch (isExerciseFinished(4)) {
+                case true: println finishedMessage
+                case false: println pressEnter
+                    playerInput()
+                    if (fileCount(folder, 'docx') == 2) {
+                        openUrl win1Gif
+                        openUrl task4Link
+                        finishExercise(4)
+                    } else {
+                        println failMessage
+                        openUrl lose2Gif
+                    }
             }
         }
 
         /* Fifth exercise */
         if (isExerciseAllowed(5)) {
-            if (isExerciseFinished(5)) {
-                /* Do something */
-            } else {
-                /* Give exercise */
-                playerInput()
-                if (fileCount(folder, 'jpg') >= 0) {
-                    /* TODO talk it over with the client */
-                    openUrl win3
-                    openUrl exercise5
-
-                    finishExercise(5)
-                }
+            println task5
+            switch (isExerciseFinished(5)) {
+                case true: finishedMessage
+                case false: println pressEnter
+                    playerInput()
+                    if (fileCount(folder, 'jpeg') == 1) {
+                        openUrl win3Gif
+                        openUrl task5Link
+                        finishExercise(5)
+                    } else {
+                        println failMessage
+                        openUrl lose3Gif
+                    }
             }
         }
 
         /*Sixth exercise */
         if (isExerciseAllowed(6)) {
-            if (isExerciseFinished(6)) {
-                /* Do something */
-            } else {
-                /* Give exercise */
-                playerInput()
-                if (fileCount(folder, 'docx') >= 0) {
-                    /* TODO talk it over with the client */
-                    openUrl win3
-                    openUrl exercise6
-
-                    finishExercise(6)
-                }
+            println task6
+            switch (isExerciseFinished(6)) {
+                case true: finishedMessage
+                case false: println pressEnter
+                    playerInput()
+                    if (fileCount(folder, 'docx') == 3) {
+                        openUrl win3Gif
+                        openUrl task6Link
+                        finishExercise(6)
+                    } else {
+                        openUrl lose3Gif
+                        println failMessage
+                    }
             }
         }
 
         /* Seventh exercise */
         if (isExerciseAllowed(7)) {
-            if (isExerciseFinished(7)) {
-                /* Do something */
-            } else {
-                /* Give exercise */
-                playerInput()
-                // TODO make a loop
-                openUrl win2
-                openUrl exercise7
-
-                finishExercise(7)
+            println task6
+            switch (isExerciseFinished(7)) {
+                case true: finishedMessage
+                case false: typoHunt(typos)
+                    openUrl win2Gif
+                    openUrl task7Link
+                    finishExercise(7)
             }
         }
-//
+
 //        /* Eight exercise */
 //        if (isExerciseAllowed(8)) {
 //            println "Akasztofa"
@@ -143,50 +147,54 @@ class BirthdayQuiz {
 //
         /* Ninth exercise */
         if (isExerciseAllowed(9)) {
-            if (isExerciseFinished(9)) {
-                /* Do something */
-            } else {
-                /* Give exercise */
-                playerInput()
-
-                openUrl win2
-                openUrl exercise9
-
-                finishExercise(9)
+            println task9
+            switch (isExerciseFinished(9)) {
+                case true: println finishedMessage
+                case false: println pressEnter
+                    if (playerInput() == code) {
+                        openUrl win2Gif
+                        openUrl task9Link
+                        finishExercise(9)
+                    } else {
+                        openUrl lose2Gif
+                        println failMessage
+                    }
             }
         }
 
         /* Tenth exercise */
         if (isExerciseAllowed(10)) {
-            if (isExerciseFinished(10)) {
-                /* Do something */
-            } else {
-                /* Give exercise */
-                playerInput()
-                if (fileCount(folder, 'jpg') >= 0) {
-
-                    openUrl win1
-                    openUrl exercise10
-
-                    finishExercise(10)
-                }
+            println task10
+            switch (isExerciseFinished(10)) {
+                case true: println finishedMessage
+                case false: println pressEnter
+                    playerInput()
+                    if (fileCount(folder, 'jpeg') == 2) {
+                        openUrl win1Gif
+                        openUrl task10Link
+                        finishExercise(10)
+                    } else {
+                        openUrl lose2Gif
+                        println failMessage
+                    }
             }
         }
 
         /* Eleventh exercise */
         if (isExerciseAllowed(11)) {
-            if (isExerciseFinished(11)) {
-                /* Do something */
-            } else {
-                if (fileCount(folder, 'jpg') >= 0) {
-                    /* Give exercise */
-                    openUrl win1
-                    openUrl win2
-                    openUrl win3
-                    openUrl exercise11
-
-                    finishExercise(11)
-                }
+            println task11
+            switch (isExerciseFinished(11)) {
+                case true: println finishedMessage
+                case false: println pressEnter
+                    playerInput()
+                    if (fileCount(folder, 'jpeg') == 3) {
+                        openUrl win3Gif
+                        openUrl task11Link
+                        finishExercise(11)
+                    } else {
+                        openUrl lose1Gif
+                        println failMessage
+                    }
             }
         }
     }
