298ecc3221e694b8989ad24358382ffc459c5d45
AlparSzabados <c3sp3nar@yahoo.com>
2016-03-29 10:02:04 +0300

    Added map to help in changing the XML values.

---
+++ src/main/groovy/birthdayQuiz/ExerciseTerminate.groovy
@@ -0,0 +1,20 @@
+package birthdayQuiz
+
+import static birthdayQuiz.exercises.ExerciseRegistry.*
+
+class ExerciseTerminate {
+
+    static EXERCISE_TERMINATE = [(_01): 'exercise01',
+                                 (_02): 'exercise02',
+                                 (_03): 'exercise03',
+                                 (_04): 'exercise04',
+                                 (_05): 'exercise05',
+                                 (_06): 'exercise06',
+                                 (_07): 'exercise07',
+                                 (_08): 'exercise08',
+                                 (_09): 'exercise09',
+                                 (_10): 'exercise10',
+                                 (_11): 'exercise11']
+
+
+}
