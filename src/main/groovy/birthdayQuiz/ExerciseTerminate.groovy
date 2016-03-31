6fa29a8e08b4b4ae4090501c1c10fae498395c62
AlparSzabados <c3sp3nar@yahoo.com>
2016-03-31 09:26:09 +0300

    Merged Classes.

--- src/main/groovy/birthdayQuiz/ExerciseTerminate.groovy
+++ src/main/groovy/birthdayQuiz/ExerciseTerminate.groovy
@@ -1,20 +1,43 @@
 package birthdayQuiz
 
+import birthdayQuiz.exercises.ExerciseRegistry
+
+import static birthdayQuiz.FolderHolder.CONFIG
+import static birthdayQuiz.XmlMaker.Status.FINISHED
 import static birthdayQuiz.exercises.ExerciseRegistry.*
+import static groovy.xml.XmlUtil.serialize
 
 class ExerciseTerminate {
 
-    static EXERCISE_TERMINATE = [(_01): 'exercise01',
-                                 (_02): 'exercise02',
-                                 (_03): 'exercise03',
-                                 (_04): 'exercise04',
-                                 (_05): 'exercise05',
-                                 (_06): 'exercise06',
-                                 (_07): 'exercise07',
-                                 (_08): 'exercise08',
-                                 (_09): 'exercise09',
-                                 (_10): 'exercise10',
-                                 (_11): 'exercise11']
+    static isExerciseFinished(ExerciseRegistry ex) {
+        EXERCISE_DONE[ex] == FINISHED
+    }
+
+    static config = new XmlSlurper().parseText(CONFIG.text)
+
+    private static exercises = config.exercises
+    protected static final EXERCISE_DONE = [(_01): exercises._01,
+                                            (_02): exercises._02,
+                                            (_03): exercises._03,
+                                            (_04): exercises._04,
+                                            (_05): exercises._05,
+                                            (_06): exercises._06,
+                                            (_07): exercises._07,
+                                            (_08): exercises._08,
+                                            (_09): exercises._09,
+                                            (_10): exercises._10,
+                                            (_11): exercises._11]
 
 
+    static finishExercise(ExerciseRegistry ex) {
+        config
+        EXERCISE_DONE.each { key, value ->
+            if (key == (ex)) {
+                config.'**'.findAll {
+                    if (it.name() == key.toString()) it.replaceBody FINISHED
+                }
+            }
+        }
+        CONFIG.text = serialize(config)
+    }
 }
