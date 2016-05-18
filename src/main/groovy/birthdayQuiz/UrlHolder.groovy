d4cafd123e2f7e6a9ca406d842646f183a22710a
AlparSzabados <c3sp3nar@yahoo.com>
2016-05-19 00:05:55 +0300

    Added more `wins` and `loses` URL-s for grater diversity.

--- src/main/groovy/birthdayQuiz/UrlHolder.groovy
+++ src/main/groovy/birthdayQuiz/UrlHolder.groovy
@@ -1,9 +1,17 @@
 package birthdayQuiz
 
 class UrlHolder {
-    static wins = ['http://giphy.com/gifs/applause-clapping-clap-aLdiZJmmx4OVW']
-    //TODO find more GIFs and make a random method
-    static loses = ['http://giphy.com/gifs/ussoccer-mexico-mnt-mntvmex-3oEdv8Qtdp1Wq4w7ok']
+    static wins = ['http://giphy.com/gifs/applause-clapping-clap-aLdiZJmmx4OVW',
+                   'http://giphy.com/gifs/football-night-fantasy-lnlAifQdenMxW',
+                   'http://giphy.com/gifs/hooray-skeletor-aWRWTF27ilPzy',
+                   'http://giphy.com/gifs/foxtv-win-ricky-gervais-emmys-2015-l41lYCDgxP6OFBruE',
+                   'http://giphy.com/gifs/win-1GTZA4flUzQI0']
+
+    static loses = ['http://giphy.com/gifs/ussoccer-mexico-mnt-mntvmex-3oEdv8Qtdp1Wq4w7ok',
+                    'http://giphy.com/gifs/christmas-fire-tree-5xtDarEqEqbHxG4A6WY',
+                    'http://giphy.com/gifs/afv-funny-fail-lol-3ornk64Apg6Ip97m5W',
+                    'http://giphy.com/gifs/fail-facepalm-lULDjDOfqVFmg',
+                    'http://giphy.com/gifs/fail-futurama-zoidberg-d1tlu8P8b5FkY']
 
     static taskLinks = ['https://www.google.com/?gws_rd=cr,ssl&ei=f1fwVrS5EYLg6ATf54CYCw&fg=1', // TODO
                         'https://www.google.com/?gws_rd=cr,ssl&ei=f1fwVrS5EYLg6ATf54CYCw&fg=1',
